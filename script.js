866caa923ab63c5da17280b2c3f0956d8d246db6
pawarharshal3336 <111567574+pawarharshal3336@users.noreply.github.com>
2024-02-18 11:11:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("add-to-cart-btn").addEventListener("click", function() {
+    // Implement functionality to add the product to the cart
+    alert("Product added to cart!");
+});
